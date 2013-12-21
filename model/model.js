a76ff6c12454b6830c3862e97d7499eb955b3a2b
Sam Anklesaria <amsay@amsay.net>
2013-12-21 10:52:38 -0600

    setup for Heroku deployment

--- model/model.js
+++ model/model.js
@@ -1,6 +1,4 @@
-exports.Sequelize = require('sequelize');
-
-exports.sequelize = new exports.Sequelize('radiotest', 'node', 'happy place', {
-	dialect: 'postgres',
-	port: 5432
-});
+// exports.sequelize = new require('sequelize')('radiotest', 'node', 'happy place', {
+// 	dialect: 'postgres',
+// 	port: 5432
+// });

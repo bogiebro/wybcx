f70f3864ad4340b25a90d471a900d378cc6700a9
Sam Anklesaria <amsay@amsay.net>
2014-01-02 10:12:36 -0600

    removing build on upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 rm -r build
 ./node_modules/bower/bin/bower install
 ./node_modules/brunch/bin/brunch build --production
-find build -type f | grep 'js\|css' | ./node_modules/LiveScript/bin/livescript s3upload.ls
+find build -type f | grep 'js\|css\|png' | ./node_modules/LiveScript/bin/livescript s3upload.ls
